42ad98dd7599daf5a4fb011d09f86a5d93dd6040
Samuel Massinon <samuel.massinon@invenia.ca>
2015-07-16 14:34:43 -0500

    Updated Mock code to use a fake FTP, rather than stub. Made the fake ftp easily accesible to the test file

--- test/MockFTPServerJulia.java
+++ test/MockFTPServerJulia.java
@@ -1,30 +1,70 @@
-import org.mockftpserver.stub.StubFtpServer;
 import org.mockftpserver.core.command.StaticReplyCommandHandler;
+import org.mockftpserver.fake.filesystem.FileEntry;
+import org.mockftpserver.fake.filesystem.FileSystem;
+import org.mockftpserver.fake.filesystem.UnixFakeFileSystem;
+import org.mockftpserver.fake.FakeFtpServer;
+import org.mockftpserver.fake.UserAccount;
+// import org.mockftpserver.stub.example.RemoteFile;
 
 public class MockFTPServerJulia
 {
 
-    private static StubFtpServer stubFtpServer;
+//    private RemoteFile remoteFile;
+    private static FakeFtpServer fakeFtpServer = new FakeFtpServer();
 
-    public static int setUp()
+    public static boolean setUp()
     {
         System.out.println("Starting server");
-        stubFtpServer = new StubFtpServer();
-        final String FEAT_TEXT = "Login successful.";
-        StaticReplyCommandHandler featCommandHandler = new StaticReplyCommandHandler(230, FEAT_TEXT);
-        stubFtpServer.setCommandHandler("AUTH", featCommandHandler);
-        stubFtpServer.start();
+        fakeFtpServer.start();
         System.out.println("Server started");
 
-        return 1;
+        int port = fakeFtpServer.getServerControlPort();
+
+//        remoteFile = new RemoteFile();
+//        remoteFile.setServer("localhost");
+//        remoteFile.setPort(port);
+
+        return true;
     }
 
-    public static int tearDown()
+    public static boolean setCommandResponse(String request, int code, String response)
+    {
+        StaticReplyCommandHandler featCommandHandler = new StaticReplyCommandHandler(code, response);
+        fakeFtpServer.setCommandHandler(request, featCommandHandler);
+
+        return true;
+    }
+
+    public static boolean setFile(String fileName, String content)
+    {
+        FileSystem fileSystem = new UnixFakeFileSystem();
+        fileSystem.add(new FileEntry(fileName, content));
+        fakeFtpServer.setFileSystem(fileSystem);
+
+        return true;
+    }
+
+    public static boolean setUser(String userName, String passowrd, String homeDir)
+    {
+        UserAccount userAccount = new UserAccount(userName, passowrd, homeDir);
+        fakeFtpServer.addUserAccount(userAccount);
+
+        return true;
+    }
+
+//    public static boolean didFileUpload(String fileName, String expected)
+//    {
+//        String actuals = remoteFile.readFile(fileName);
+//
+//        return expected.equals(actuals);
+//    }
+
+    public static boolean tearDown()
     {
         System.out.println("Stoping server");
-        stubFtpServer.stop();
+        fakeFtpServer.stop();
         System.out.println("Server started");
 
-        return 1;
+        return true;
     }
 }
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,26 +6,44 @@ using JavaCall
 
 # Start Java, use verbose for debug and point to the class in this directory
 JavaCall.init(["-verbose:jni", "-verbose:gc","-Djava.class.path=$(joinpath(pwd(), "test"))"])
+MockFTPServerJulia = @jimport MockFTPServerJulia
 
 function start_server()
-    MockFTPServerJulia = @jimport MockFTPServerJulia
-    jcall(MockFTPServerJulia, "setUp", jint, ())
+    result = jcall(MockFTPServerJulia, "setUp", jboolean, ())
+    @assert result == 1 "start_server failed"
 end
 
 function stop_server()
-    MockFTPServerJulia = @jimport MockFTPServerJulia
-    jcall(MockFTPServerJulia, "tearDown", jint, ())
+    result = jcall(MockFTPServerJulia, "tearDown", jboolean, ())
+    @assert result == 1 "stop_server failed"
+end
+
+function set_user(name::String, passowrd::String, home_dir::String)
+    result = jcall(MockFTPServerJulia, "setUser", jboolean, (JString, JString, JString), name, passowrd, home_dir)
+    @assert result == 1 "set_user failed"
+end
+
+function set_file(name::String, content::String)
+    result = jcall(MockFTPServerJulia, "setFile", jboolean, (JString, JString), name, content)
+    @assert result == 1 "set_file failed"
+end
+
+function set_command_response(request::String, code::Int64, reponse::String)
+    result = jcall(MockFTPServerJulia, "setCommandResponse", jboolean, (JString, jint, JString,), request, code, reponse)
+    @assert result == 1 "set_command_response failed"
 end
 
 
 # write your own tests here
 @test 1 == 1
 
-# Test run
+# Test mock
+set_user("test", "test", "/")
+set_file("/test_file.txt", "hello, world")
+set_command_response("AUTH", 230, "Login successful.")
 start_server()
+
+
+
 stop_server()
-
-
-
-
 JavaCall.destroy()
