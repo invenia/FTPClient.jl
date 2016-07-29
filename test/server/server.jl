60a259e1e14669d339331c4a99c5eada91dd5645
Sam Morrison <sam.morrison@invenia.ca>
2016-07-29 08:18:18 -0500

    server.jl can now use ssl

--- test/server/server.jl
+++ test/server/server.jl
@@ -2,6 +2,9 @@ import Base: Process
 
 const ROOT = abspath(dirname(@__FILE__), "root")
 const SCRIPT = abspath(dirname(@__FILE__), "server.py")
+const CERT = abspath(dirname(@__FILE__), "test.crt")
+const KEY = abspath(dirname(@__FILE__), "test.key")
+
 
 type FTPServer
     process::Process
@@ -9,14 +12,20 @@ type FTPServer
     port::Int
     root::AbstractString
 
-    function FTPServer(root::AbstractString=ROOT)
+     function FTPServer(root::AbstractString=ROOT; ssl::AbstractString="nothing")
         # Note: open(::AbstractCmd, ...) won't work here as it doesn't allow us to capture STDERR.
         io = Pipe()
-        process = spawn(pipeline(`python $SCRIPT user:passwd:$root:elradfmwM`, stdout=io, stderr=io))
-        m = match(r"starting FTP server on .*:(?<port>\d+)", readline(io))
+
+        process = (isequal(ssl, "nothing"))? spawn(pipeline(`python $SCRIPT user:passwd:$root:elradfmwM`, stdout=io, stderr=io)):
+                spawn(pipeline(`python $SCRIPT --tls $ssl --cert-file $CERT --key-file $KEY user:passwd:$root:elradfmwM`, stdout=io, stderr=io))
+
+        line = readline(io)
+        println(line)
+        m = match(r"starting FTP.* server on .*:(?<port>\d+)", line)
         port = parse(Int, m[:port])
         new(process, io, port, root)
     end
+
 end
 
 port(server::FTPServer) = server.port
@@ -45,7 +54,7 @@ function setup_root(dir::AbstractString)
 end
 
 function setup_server()
-    isdir(ROOT) || setup_root(ROOT)
+    isdir(joinpath(ROOT, "test_directory")) || setup_root(ROOT)
     generate_self_signed("test", dirname(@__FILE__))
 end
 
