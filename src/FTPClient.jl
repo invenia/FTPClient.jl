9adf9aee6299be7712f30641a1a7bcefbf201328
Curtis Vogt <curtis.vogt@gmail.com>
2017-06-13 19:29:09 -0500

    Revise Compat usage

--- src/FTPClient.jl
+++ src/FTPClient.jl
@@ -1,15 +1,8 @@
 module FTPClient
 
-using Compat
-
-if VERSION >= v"0.5-"
-    typealias RemoteRef Future
-else
-    typealias unsafe_write write
-end
-
 import Base: convert, show, open, mkdir, ascii, mv
 import Base: readdir, cd, pwd, rm, close, download
+import Compat: readstring, unsafe_write
 
 type FTPClientError <: Exception
     msg::AbstractString
