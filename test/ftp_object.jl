7206bd8ef653534b75acb6c17a05f49a5e5c870d
Adrienne Pind <adriennepind@gmail.com>
2018-01-23 10:40:41 -0600

    Un-comment out url test

--- test/ftp_object.jl
+++ test/ftp_object.jl
@@ -66,12 +66,12 @@ end
     close(ftp)
 end
 
-# @testset "connection with url" begin
-#     host = hostname(server)
-#     ftp = FTP(; url="ftp://$host/", opts...)
-#     @test ftp.ctxt.url == "ftp://$host/"
-#     close(ftp)
-# end
+@testset "connection with url" begin
+    host = hostname(server)
+    ftp = FTP(; url="ftp://$host/", opts...)
+    @test ftp.ctxt.url == "ftp://$host/"
+    close(ftp)
+end
 
 @testset "readdir" begin
     # check readdir
