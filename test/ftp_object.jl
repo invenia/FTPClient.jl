d112d1b15de7d46727cd11ef7444609aecac3921
Adrienne Pind <adriennepind@gmail.com>
2018-01-29 10:06:44 -0600

    Check if file exists before removing

--- test/ftp_object.jl
+++ test/ftp_object.jl
@@ -400,7 +400,7 @@ end
             close(io)
             @test filesize(temp_file_path) > 0
         finally
-            rm(temp_file_path)
+            isfile(temp_file_path) && rm(temp_file_path)
             ftp_cleanup()
         end
     end
