df7325a8420209892ca1c1181aa9c5ed6afe2666
Curtis Vogt <curtis.vogt@gmail.com>
2019-01-09 13:45:16 -0600

    Superficial corrections

--- src/conn_context.jl
+++ src/conn_context.jl
@@ -107,7 +107,7 @@ end
 Download a file with a persistent connection. Returns a `Response`.
 
 # Arguments
-* `ctxt::ConnContext`: encompases the connection options defined via ftp_connect. See
+* `ctxt::ConnContext`: encompasses the connection options defined via ftp_connect. See
     `RequestOptions` for details.
 * `file_name::AbstractString`: the path to the file on the server.
 * `save_path::AbstractString=""`: if not specified the file is written to the `Response`
